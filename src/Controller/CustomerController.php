c894fa353e6b60ec461e4e96f6e55bfbfe2e1124
Sebastien Dru <sebdru.fr@gmail.com>
2022-11-02 13:06:17 +0100

    issue #8 done

--- src/Controller/CustomerController.php
+++ src/Controller/CustomerController.php
@@ -13,6 +13,7 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
+use Symfony\Component\Serializer\Normalizer\AbstractNormalizer;
 use Symfony\Component\Serializer\SerializerInterface;
 
 #[Route('/api', name: 'api_')]
@@ -74,17 +75,21 @@ class CustomerController extends AbstractController
     #[Route('/users/{userId}/customers/{customerId}', name:"createCustomer", methods: ['PUT'])]
     public function updateCustomer(int $userId, int $customerId,UserRepository $userRepository, CustomerRepository $customerRepository, Request $request, SerializerInterface $serializer, EntityManagerInterface $entityManager, UrlGeneratorInterface $urlGenerator): JsonResponse
     {
+
         $user = $userRepository->findBy(['id' => $userId]);
-        $customer = $customerRepository->findOneBy(['id' => $customerId, "users" => $user]);
-        $updateCustomer = $serializer->deserialize($request->getContent(), Customer::class, 'json');
-        $customer->setUsers($user);
-        $entityManager->persist($customer);
+        $currentCustomer = $customerRepository->findOneBy(['id' => $customerId, "users" => $user]);
+
+        if (empty($currentCustomer)) {
+            return new JsonResponse(null, Response::HTTP_NOT_FOUND);
+        }
+        $updatedCustomer = $serializer->deserialize($request->getContent(),
+            Customer::class,
+            'json',
+            [AbstractNormalizer::OBJECT_TO_POPULATE => $currentCustomer]);
+
+        $entityManager->persist($updatedCustomer);
         $entityManager->flush();
 
-        $jsonCustomer = $serializer->serialize($customer, 'json', ['groups' => 'getCustomers']);
-
-        $location = $urlGenerator->generate('api_customerDetails', ['customerId' => $customer->getId(), 'userId' => $user->getId()], UrlGeneratorInterface::ABSOLUTE_URL);
-
-        return new JsonResponse($jsonCustomer, Response::HTTP_CREATED, ["Location" => $location], true);
+        return new JsonResponse(null, JsonResponse::HTTP_NO_CONTENT);
     }
 }
